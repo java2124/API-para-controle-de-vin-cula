25a10c613aa04175c27b5badef5c97015adcc652
java2124 <rafabf1508@gmail.com>
2021-01-18 18:08:22 -0300

    first commit

---
+++ ProjetoVinhos/Models/Vinhos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoVinhos.Models
+{
+    public class Vinhos
+    {
+        public int cod_vinho { get; set; }
+
+        [Required]
+        [StringLength (100, ErrorMessage = "Campo Inválido!", MinimumLength = 4)]
+        public string nome_vinho { get; set; }
+        public int idade_vinho { get; set; }
+        [DataType(DataType.Currency)] public decimal preco_vinho { get; set; }
+    }
+}
